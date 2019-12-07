045870919de8346ad6618ebf469b57c8f0ce2a1b
jooyong <pzy1108@naver.com>
2019-12-07 11:15:12 +0900

    베인 배경음악 사운드 재수정
    물방울 발사 시 메인 음악이 꺼지는 오류가 있어서 main bgm을 다시 main.py의 enter안으로 넣음

--- grass.py
+++ grass.py
@@ -4,9 +4,7 @@ class Grass:
     def __init__(self):
         self.image = load_image('C:\\2017180012 jpark\\2017180012_2DGP_MyGame\\res\\grass.png')
         self.x, self.y = 400, 30
-        self.bgm_main = load_music('C:\\2017180012 jpark\\2017180012_2DGP_MyGame\\sound\\happy2.mp3')
-        self.bgm_main.set_volume(64)
-        self.bgm_main.repeat_play()
+
 
 
     def update(self):
